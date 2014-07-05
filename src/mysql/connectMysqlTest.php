d7ef494eb6bec3f9d9db954078c001ab73479832
Jayin Ton <tonjayin@gmail.com>
2014-07-05 21:03:48 +0800

    test on mysql connect~

---
+++ src/mysql/connectMysqlTest.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../mylibs/_.php';
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "test";
+$port = 3307;
+$socket = null;
+#connect ,on this oop way
+$mysqli = new mysqli($host, $user, $password, $database, $port, $socket);
+
+if($mysqli->connect_errno){
+    d("connect error");
+    dpf($mysqli->connect_error);
+}
+
+dpf($mysqli);
