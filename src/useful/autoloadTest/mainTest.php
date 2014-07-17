7430a4501913df5d521e5cd76b608be7f91a16bf
Jayin Ton <tonjayin@gmail.com>
2014-07-17 10:54:47 +0800

    [add]spl_autoload_extension()

--- src/useful/autoloadTest/mainTest.php
+++ src/useful/autoloadTest/mainTest.php
@@ -12,7 +12,9 @@ function autoLoader($class){
 }
 
 spl_autoload_register("autoLoader");
-
+//注册并返回spl_autoload函数使用的默认文件扩展名,mostly is .php file
+echo spl_autoload_extensions('.php');
 new Apple();
 
 new Orange();
+
